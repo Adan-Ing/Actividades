3bf31342df7bc4962c47070c1faac2148672592e
Adán <joge_22@alu.uabcs.mx>
2024-10-19 22:07:13 -0700

    Update index.js

--- u4_Ejercicio2/js/index.js
+++ u4_Ejercicio2/js/index.js
@@ -1,5 +1,3 @@
-console.log('El archivo JavaScript está cargado correctamente.');
-
 function cambiarColor(color) {
     document.body.style.backgroundColor = color;
     document.querySelector('.container').style.backgroundColor = color === 'white' ? 'white' : 'rgba(255, 255, 255, 0.8)';

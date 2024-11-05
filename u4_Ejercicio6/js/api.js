153f84ff28c2b35de257bb39a8ca347b1c389f73
Adán <joge_22@alu.uabcs.mx>
2024-11-04 21:21:52 -0700

    Update api.js

--- u4_Ejercicio6/js/api.js
+++ u4_Ejercicio6/js/api.js
@@ -2,31 +2,37 @@ const options = {
     headers: {
         'x-api-key': 'live_9xAXeOySuZF12pqsDZzqNZy3lMzRgjHdcdFDViMWLt5kIahrWeNrkjQZ5QwzVkWA'
     }
-};
+}
+const apiUrl = "https://api.thecatapi.com/v1/breeds";
+const apiUrlDescription = 'https://api.thecatapi.com/v1/images';
 
-export async function getBreeds(page, limit) {
-    const apiUrl = "https://api.thecatapi.com/v1/breeds";
+export async function getBreeds(page, limit){
     try {
         const response = await fetch(`${apiUrl}?limit=${limit}&page=${page - 1}`, options); 
-        if (!response.ok) {
-            throw new Error("Error " + response.status);
+        if(!response.ok){
+            throw new Error("Error " + response.status)
         }
+        
         return await response.json();
-    } catch (error) {
+    }catch (error) {
         console.error(error);
     }
 }
 
 export async function getBreedDetails(id) {
-    const apiUrl = 'https://api.thecatapi.com/v1/images';
-    try {
-        const response = await fetch(`${apiUrl}/${id}`, options);
-        if (!response.ok) {
+    try{
+        const url = `${apiUrlDescription}/${id}`;
+        console.log(url)
+        const response = await fetch(url, options);
+
+        if(!response.ok){
             const errorMessage = await response.text();
-            throw new Error('Error: ' + errorMessage);
+            throw new Error('Error: ' + errorMessage)
         }
+
         return await response.json();
-    } catch (error) {
+        
+    }catch(error){
         console.log(error);
     }
 }
